f00573b52b55d354813c8437d5f4dfbd844e05a0
Denis <hodenis23@gmail.com>
2021-01-11 20:43:57 +0200

    Готово

---
+++ Lesson while.py
@@ -0,0 +1,12 @@
+def func():
+    a=1
+    b=10
+
+    while a <= b:
+        print("a все еще не равно b")
+        a = a + 1
+        print("A + 1 =", a)
+    print("Сравнение закончили")
+    print("a больше b")
+
+func()
